3f3d5e464143eb9354531e2a1da12364e14fb90b
aisharahimanl <78393688+aisharahimanl@users.noreply.github.com>
2021-03-28 14:44:23 -0400

    a3

--- A3/assignment3/app.py
+++ A3/assignment3/app.py
@@ -1,6 +1,16 @@
 import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import sqlite3
+
+
+def get_db():
+    
+    db = getattr(g, '_database', None)
+    if db is None:
+        
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 app = Flask(__name__)
 
